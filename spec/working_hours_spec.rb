b3aca21f409da6a34f2768136775eee764fb5c83
Jon Grimes <jonkgrimes@gmail.com>
2014-01-11 08:36:42 -0600

    Completed specs

--- spec/working_hours_spec.rb
+++ spec/working_hours_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe WorkingHours do
+
   describe ".weekend?" do
     context "on the weekend" do
       it "should return true" do
-        time = DateTime.new(2013,10,11,19)
+        time = DateTime.new(2013,10,11,19,0,0,"-6")
         WorkingHours.weekend?(time).should be_true
+      end
+    end
+
+    context "during business hours on a weekday" do
+      it "should return false" do
+        time = DateTime.new(2013,10,11,10,0,0,"-6")
+        WorkingHours.weekend?(time).should be_false
       end
     end
   end
@@ -13,11 +21,28 @@ describe WorkingHours do
   describe ".after_hours?" do
     context "before the business opens" do
       context "on a weekday" do
-        it "should return true"  
+        it "should return true" do
+          time = DateTime.new(2013,10,11,7,0,0,"-6")
+          WorkingHours.after_hours?(time).should be_true
+        end
       end
+    end
 
-      context "on a weekend" do
-        it "should return true"
+    context "during business hours" do
+      context "on a weekday" do
+        it "should return false" do
+          time = DateTime.new(2013,10,11,10,0,0,"-6")
+          WorkingHours.after_hours?(time).should be_false
+        end
+      end
+    end
+
+    context "after the business closes" do
+      context "on a weekday" do
+        it "should return true" do
+          time = DateTime.new(2013,10,11,19,0,0,"-6")
+          WorkingHours.after_hours?(time).should be_true
+        end
       end
     end
   end
